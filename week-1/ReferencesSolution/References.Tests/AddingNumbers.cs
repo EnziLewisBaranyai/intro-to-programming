383d8a91243aaeebf225355536dbe36113609ca9
Enzi Lewis-Baranyai <Enzi_K_Lewis-Baranyai@Progressive.com>
2025-07-07 16:30:20 -0400

    Add test files

---
+++ week-1/ReferencesSolution/References.Tests/AddingNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace References.Tests;
+
+public class AddingNumbers
+{
+    [Fact]
+    public void CanAddTwoNumbers()
+    {
+        int a = 10;
+        int b = 20;
+        int answer;
+
+        answer = a + b;
+
+        Assert.Equal(30, answer);
+    }
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(2, 2, 4)]
+    [InlineData(10, 3, 13)]
+    public void CanAddAnyTwoIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+        Assert.Equal(expected, answer);
+    }
+}
