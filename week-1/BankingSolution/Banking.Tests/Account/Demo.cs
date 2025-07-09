b55bf38c23f7c6c0439c184ed4a7b00d6183a90e
Enzi Lewis-Baranyai <Enzi_K_Lewis-Baranyai@Progressive.com>
2025-07-09 14:30:47 -0400

    Fix

---
+++ week-1/BankingSolution/Banking.Tests/Account/Demo.cs
@@ -0,0 +1,30 @@
+﻿using Banking.Domain;
+
+namespace Banking.Tests.Account;
+
+public class Demo
+{
+    [Fact]
+    public void SomeDemo() 
+    {
+        var barbsAccount = new BankAccount();
+        //BankAccount barbsAccount = new();
+
+        var stansAccount = new BankAccount();
+
+        Assert.Equal(barbsAccount.GetBalance(), stansAccount.GetBalance());
+
+        stansAccount.Deposit(420.69M);
+
+        Assert.NotEqual(barbsAccount.GetBalance(), stansAccount.GetBalance());
+    }
+
+    [Fact]
+    public void SomeOtherDemo()
+    {
+        var myBirthday = new DateTime(1969, 4, 20);
+        var barbsBirthday = new DateTime(1974, 4, 10);
+
+        int myPay = 10;
+    }
+}
